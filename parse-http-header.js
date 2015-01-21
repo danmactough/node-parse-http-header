f028bca895abec852cbd0766a13de5ad70addd32
James Newell <james@digitaledgeit.com.au>
2015-01-22 04:29:21 +1100

    fixed some failing edge cases and added some tests

--- parse-http-header.js
+++ parse-http-header.js
@@ -1,20 +1,34 @@
 function Param (string) {
   var SEPARATOR = /(?:;|,)/g
     , OPERATOR = /=/;
-  if (string && SEPARATOR.test(string) && OPERATOR.test(string)) {
-    string.split(SEPARATOR).forEach(function (param, idx) {
-      var parts = param.split(OPERATOR).map(function (part) { return part.trim(); });
-      if (parts.length === 2) {
-        this[parts[0]] || (this[parts[0]] = {});
-        this[parts[0]] = parts[1];
-        this[idx] = {};
-        this[idx][parts[0]] = parts[1];
-      }
-      else {
-        this[idx] = param;
-      }
-    }, this);
+  if (string && SEPARATOR.test(string)) {
+
+    var split = string.split(SEPARATOR);
+
+    if (OPERATOR.test(string)) {
+      split.forEach(function (param, idx) {
+        var parts = param.split(OPERATOR).map(function (part) {
+          return part.trim();
+        });
+        if (parts.length === 2) {
+          this[parts[0]] || (this[parts[0]] = {});
+          this[parts[0]] = parts[1];
+          this[idx] = {};
+          this[idx][parts[0]] = parts[1];
+        }
+        else {
+          this[idx] = param;
+        }
+      }, this);
+
+    } else {
+      this[0] = split[0];
+    }
+
+  } else {
+    this[0] = string;
   }
+
   return this;
 }
 
